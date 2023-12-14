c423752d8958593310d73045935f58fe856c6700
Siddharth Panditrao <115323514+SoloSid69@users.noreply.github.com>
2023-12-14 14:54:52 +0530

    Different CNN

---
+++ Cat and Dog Image Classifier V2.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import Sequential
+from keras.layers import Dense,Conv2D,MaxPooling2D,Flatten,BatchNormalization,Dropout
+
+#Dividing Data into small Batches (Generators)
+train_ds = keras.utils.image_dataset_from_directory(
+    directory = '(path to training set)',
+    labels = 'inferred',
+    label_mode = 'int',
+    batch_size = 32,
+    image_size = (256,256)
+)
+
+validation_ds = keras.utils.image_dataset_from_directory(
+    directory = '(path to Testing set)',
+    labels = 'inferred',
+    label_mode = 'int',
+    batch_size = 32,
+    image_size = (256,256)
+)
+
+#Normalize the Data:
+def process(image,label):
+  image = tf.cast(image/255. ,tf.float32)
+  return image,label
+
+train_ds = train_ds.map(process)
+validation_ds = validation_ds.map(process)
+
+#Creating a CNN model
+#Approach 1: Focus on activation functions and pooling:
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), padding='valid', activation='leaky_relu', input_shape=(256, 256, 3)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='valid'))
+
+model.add(Conv2D(64, kernel_size=(3, 3), padding='valid', activation='leaky_relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='valid'))
+
+model.add(Conv2D(128, kernel_size=(3, 3), padding='valid', activation='leaky_relu'))
+model.add(BatchNormalization())
+model.add(AveragePooling2D(pool_size=(2, 2), strides=2, padding='valid'))  # Change from MaxPooling to AveragePooling
+
+model.add(Flatten())
+
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(1, activation='sigmoid'))
+
+
+model.summary()
+
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+history = model.fit(train_ds,epochs=10,validation_data=validation_ds)
+
+import matplotlib.pyplot as plt
+
+#Accuracy Plot
+plt.plot(history.history['accuracy'],color='red',label='train')
+plt.plot(history.history['val_accuracy'],color='blue',label='validation')
+plt.legend()
+plt.show()
+
+#Loss Plot
+plt.plot(history.history['loss'],color='red',label='train')
+plt.plot(history.history['val_loss'],color='blue',label='validation')
+plt.legend()
+plt.show()
+
+#Testing out with Random Image:
+import cv2
+test_image = cv2.imread('(Path to Test Image)')
+plt.imshow(test_image)
+test_image.shape
+test_image=cv2.resize(test_image,(256,256))
+test_input = test_image.reshape(1,256,256,3)
+model.predict(test_input)
